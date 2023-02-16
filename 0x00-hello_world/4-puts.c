552db9f6f2ffcadb3323f58ebf876ee9eb495ae9
Adeyemi Oluwagbenga Daniel <adeyemidanielgad@gmail.com>
2023-02-16 16:01:02 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-int main(void)
-{
-       	puts("\"Programming is like building a multilingual puzzle\n");
-	return (0); 
+int main()
+{	
+	char message[] = "\"Programming is like building a multilingual puzzle";
+       	puts(message);
+	return 0; 
 }
