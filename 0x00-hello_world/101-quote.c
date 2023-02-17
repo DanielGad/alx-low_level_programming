553bb22d194e8b75336fb78d89e4164d3f8a98ff
Adeyemi Oluwagbenga Daniel <adeyemidanielgad@gmail.com>
2023-02-17 21:54:16 +0000

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Description: This is where the function program runs.
+ *
+ * Return: 0 (success)
+ */
 int main(void)
 {
 	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
