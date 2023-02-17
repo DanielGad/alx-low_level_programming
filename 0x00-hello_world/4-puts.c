8758a259513aa4a21fb73af1bd997c757112e6de
Adeyemi Oluwagbenga Daniel <adeyemidanielgad@gmail.com>
2023-02-17 11:08:27 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-
-int main()
+/*
+ * My first C comment.
+ */
+int main(void)
 {	
 	char message[] = "\"Programming is like building a multilingual puzzle";
-       	puts(message);
-	return 0; 
+	puts(message);
+	return (0); 
 }
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int main() {
+	printf("Size of char: %ld byte(s)\n", sizeof(char));
+	printf("Size of short: %ld byte(s)\n", sizeof(short));
