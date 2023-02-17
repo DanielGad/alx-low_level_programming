cfb8dd5b435dcb87c1ea67cded86445c9b6dff41
Adeyemi Oluwagbenga Daniel <88782647+DanielGad@users.noreply.github.com>
2023-02-17 22:44:40 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,6 @@ int main(void)
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	printf("size of a float: %zu byte(s)\n", sizeof(float));
+	printf("size of a float: %zu byte(s)", sizeof(float));
 	return (0);
 }
