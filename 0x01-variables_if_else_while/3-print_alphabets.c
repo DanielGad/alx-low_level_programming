08e288db276c0667118f44a77098e9b9b07869ce
Adeyemi Oluwagbenga Daniel <adeyemidanielgad@gmail.com>
2023-02-17 23:21:51 +0000

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char n = 'a';
+
+	while (n <= 'z')
+	{
+		printf("%c ", n);
+		n++;
+	}
+	return (0);
+}
