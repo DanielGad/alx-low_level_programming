4c90ac456067287050b289ed8938f874683f9e99
Adeyemi Oluwagbenga Daniel <adeyemidanielgad@gmail.com>
2023-02-17 21:22:34 +0000

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+/**
+ *main - Entry point
+ *
+ *Description: This id where the function program runs.
+ *
+ *Return: 0 (success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
