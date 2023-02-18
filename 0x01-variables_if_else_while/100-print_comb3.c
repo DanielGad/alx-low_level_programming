eba45860ec2457f1c6dd05b87e0f54124d3cc1aa
Adeyemi Oluwagbenga Daniel <adeyemidanielgad@gmail.com>
2023-02-18 23:09:34 +0000

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,14 +6,30 @@
  */
 int main(void)
 {
-	int a;
+	int c;
 
-	while (a = 0; a <= '99')
+	int d = 0;
+
+	while (d < 10)
 	{
-		if ((a != '01' && a != '10') && a <= '99')
-		putchar(a);
-		a++;
-		putchar(", ");
+		c = 0;
+		while (c < 10)
+		{
+			if (d != c && d < c)
+			{
+				putchar('0' + d);
+				putchar('0' + c);
+
+				if (c + d != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			c++;
+		}
+		d++;
 	}
+	putchar('\n');
 	return (0);
 }
