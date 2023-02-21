40f4715754e4a410acb57b2fdef169619ba23716
Adeyemi Oluwagbenga Daniel <adeyemidanielgad@gmail.com>
2023-02-22 00:12:38 +0100

    2-print_alphabet_x10.c[D[C[C[C[C[C[C.c[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[3~[[C1[D[D1[C[D-[3~[3~[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - prints the alphabets in lowercase.
  *
  * Return: Always 0.
  */
 void print_alphabet(void)
 {
 	char letter;
+
 	for(letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
 		_putchat('\n');
