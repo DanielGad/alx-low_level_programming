6fde1017543a3e21387920242bfccbc5a2a271de
Adeyemi Oluwagbenga Daniel <adeyemidanielgad@gmail.com>
2023-02-23 13:44:41 -0800

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Test if positive or negative
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	if (i > 0)
+		printf("%d is positive\n");
+	if (i < 0)
+		printf("%d is negative\n");
+
+	return (0);
+}
+
