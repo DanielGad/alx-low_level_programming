c49b5fd518005199991b4a04f60999602bc28ac1
Adeyemi Oluwagbenga Daniel <adeyemidanielgad@gmail.com>
2023-02-24 17:36:59 -0800

    commits

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,17 @@
+ 16 lines (12 sloc)  189 Bytes
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
 
 int main(void)
 {
 	int i;
 
-	i = 58;
+	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
