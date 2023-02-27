500297478aeec548901a4d9e89b40954bc5d19ab
DanielGad <adeyemidanielgad@gmail.com>
2023-02-27 22:23:28 +0000

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,5 +8,28 @@
  */
 int _atoi(char *s)
 {
-	int i;
+	int num = 0, sign = 1, i = 0;
+
+	if (s[0] == '-')
+	{
+	sign = -1;
+	i++;
+	}
+	else if (s[0] == '+')
+	{
+		i++;
+	}
+	while (s[i] != '\0')
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			num = num * 10 + (s[i] - '0');
+			i++;
+		}
+		else
+	{
+			break;
+		}
+	}
+	return (num * sign);
 }
