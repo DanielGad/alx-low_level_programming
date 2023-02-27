af58f45c53eb6a0700dffaf26e149ff42d2b4dc0
Adeyemi Oluwagbenga Daniel <adeyemidanielgad@gmail.com>
2023-02-27 05:08:11 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * swap_int: Entry point
+ * 'a': An integer a
+ * 'b': An integer b
+ * Return: 0 (success)
+ */
+void swap_int(int *a, int *b)
+{
+	int swap = *a;
+	*a = *b;
+	*b = swap;
+	
+}
