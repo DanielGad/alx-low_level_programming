ea6754ebcd65d9b085506a1fcdfd4b0539c82f40
DanielGad <adeyemidanielgad@gmail.com>
2023-03-08 14:21:27 +0000

    update

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -5,7 +5,7 @@
  * @s1: input
  * @s2: input
  *
- * Description: function that compares two stringd and returns 1 if the strings can be considered identical.
+ * Description: function that compares two string
  * Return: 1 if the strings are identical, otherwise 0
  */
 int wildcmp(char *s1, char *s2)
