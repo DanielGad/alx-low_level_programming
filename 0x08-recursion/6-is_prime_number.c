6fa43a7374a1f2a91827ad2fea8c8592a3bd6086
Adeyemi Oluwagbenga Daniel <88782647+DanielGad@users.noreply.github.com>
2023-03-09 12:36:58 +0100

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,9 +3,9 @@
 int actual_prime(int n, int i);
 
 /**
- * is_prime_number - says if an integer is a prime number or not
- * @n: number to evaluate
- *  Return: 1 if n is a prime number, 0 if not
+ * is_prime_number - Entry point
+ * @n: input
+ *  Return: 1 if n is a prime number, otherwise 0.
  */
 int is_prime_number(int n)
 {
@@ -15,10 +15,10 @@ int is_prime_number(int n)
 }
 
 /**
- * actual_prime - calculates if a number is prime recursively
- * @n: number to evaluate
- * @i: iterator
- * Return: 1 if n is prime, 0 if not
+ * actual_prime - Entry point
+ * @n: input
+ * @i: input
+ * Return: 1 if n is prime, otherwise 0.
  */
 int actual_prime(int n, int i)
 {
