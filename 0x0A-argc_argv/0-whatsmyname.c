6af48ebfea8c239c1e1654397632d775b2a1cf8b
Adeyemi Oluwagbenga Daniel <adeyemidanielgad@gmail.com>
2023-03-10 16:22:40 -0800

    commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: input
+ * @argv: Arguement
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
