5bfb78d9866cba8180890c448e9324218378993e
Adeyemi Oluwagbenga Daniel <adeyemidanielgad@gmail.com>
2023-03-10 16:32:24 -0800

    commit

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: input
+ * @argv: input
+ *
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	int count = 0;
+	if (argc > 0)
+		(while (count < argc)
+		 (printf("%s\n", argv[count]);
+		  count++;
+		  }
+		  }
+		  return (0);
+}
