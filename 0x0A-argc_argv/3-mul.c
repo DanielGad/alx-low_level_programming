d68dc17062853b26233fd7a1bf843e7926d7f003
Adeyemi Oluwagbenga Daniel <88782647+DanielGad@users.noreply.github.com>
2023-03-14 22:24:17 +0100

    updates

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: argumemnt count
+ * @argv: argument string array
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int prod;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		prod = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", prod);
+
+		return (0);
+	}
+}
