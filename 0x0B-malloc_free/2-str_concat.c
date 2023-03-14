6feeae1a117c017a03410b55431dd47f1c86fc75
Adeyemi Oluwagbenga Daniel <88782647+DanielGad@users.noreply.github.com>
2023-03-14 23:09:47 +0100

    updates

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - does something
+ * @s1: the first string
+ * @s2: the second string
+ * Return: s3 (success)
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *s3;
+	unsigned int i = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	s3 = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
+
+	if (s3 == NULL)
+		return (NULL);
+
+	while ((s3[i] = s1[i]) != '\0')
+		i++;
+
+	s3 = strcat(s3, s2);
+
+	return (s3);
+}
