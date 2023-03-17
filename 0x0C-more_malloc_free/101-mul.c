321409cc0c9f2df3b6877a76a277fbb5b35f5b19
Adeyemi Oluwagbenga Daniel <adeyemidanielgad@gmail.com>
2023-03-16 18:09:45 -0700

    101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * Description - A program that multiplies two positive numbers.
+ * Return: (success) otherwise 0
+ */
+int main(void)
+{
+	int i, j;
+
+	i = j = 0;
+	while (i < l)
+	{
+		if (str[i] != '0')
+			j = 1;
+		if (j || i == l - 1)
+			_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+	free(str)
+}
