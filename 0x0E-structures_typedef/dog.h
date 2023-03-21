b3ed02c71399a3333dc5a1453627e9f75de19c2b
Adeyemi Oluwagbenga Daniel <88782647+DanielGad@users.noreply.github.com>
2023-03-21 21:04:17 +0100

    Dog Task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,28 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - basic structure of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+int _putchar(char);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /* DOG_H */
