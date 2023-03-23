17b5f5cd9f2d99a42cac0d86a7aa8a781991e084
Adeyemi Oluwagbenga Daniel <adeyemidanielgad@gmail.com>
2023-03-22 21:32:04 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 
 /**
- * print_name - prints name
- * @name: pointer to the name
- * @f: pointer to the function
+ * print_name - Entry point
+ * @name: input pointer to name
+ * @f: input pointer to the function
  */
 void print_name(char *name, void (*f)(char *))
 {
