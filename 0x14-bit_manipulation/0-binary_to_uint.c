b94f5ba02bd6ba0ea0bf653e1fa71d651e34fde6
Adeyemi Oluwagbenga Daniel <adeyemidanielgad@gmail.com>
2023-04-09 15:53:35 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,18 +8,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int dec = 0;
+	int j;
+	unsigned int dec_val = 0;
 
 	if (!b)
 		return (0);
 
-	for (i = 0; b[i]; i++)
+	for (j = 0; b[j]; j++)
 	{
-		if (b[i] < '0' || b[i] > '1')
+		if (b[j] < '0' || b[j] > '1')
 			return (0);
-		dec = 2 * dec + (b[i] - '0');
+		dec_val = 2 * dec_val + (b[j] - '0');
 	}
 
-	return (dec);
+	return (dec_val);
 }
