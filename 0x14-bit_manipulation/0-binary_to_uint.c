8ce8acd0a727796cfc4b3435d25ee25758378510
Adeyemi Oluwagbenga Daniel <adeyemidanielgad@gmail.com>
2023-05-04 12:52:33 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,1 +1,23 @@
 #include "main.h"
+
+/**
+ * binary_to_uint - convert a binary number
+ * @b: string input
+ * Return: the converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int jj;
+	unsigned int dec_val = 0;
+	if (!b)
+		return (0);
+
+	for (jj = 0; b[jj]; jj++)
+	{
+		if (b[jj] < '0' || b[jj] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[jj] - '0');
+	}
+	return (dec_val);
+}
+
